3e6295ff8c6a53ee172cdf2b30c7d23d109de5e0
artur <artur.kulchenko@pm.me>
2019-03-14 15:44:15 +0100

    rspec confs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,22 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'rspec/rails'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    with.library :rails
+  end
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.full_backtrace = false
+  config.failure_color = :cyan
+  config.detail_color = :yellow
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -44,8 +58,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
