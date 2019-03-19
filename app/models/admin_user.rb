2e915f5dcf42e7333c7786a7e5aeadecdbc8c556
artur <artur.kulchenko@pm.me>
2019-03-19 14:47:12 +0100

    add factorybot for admin_user/ abstracting User class

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,5 @@
 class AdminUser < User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  self.table_name = "admin_users"
   devise :database_authenticatable,
     :recoverable, :rememberable, :validatable
 end
--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
+  sequence(:email) { |n| "user.number-#{n+n}@example.com" }
   factory :admin_user do
-    
+    email
+    password '12345678'
+    password_confirmation '12345678'
   end
 end
