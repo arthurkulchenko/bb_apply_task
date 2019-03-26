d733351c377b07c5435f701b62849f79baeeb7f1
artur <artur.kulchenko@pm.me>
2019-03-26 15:32:15 +0100

    delete unneeded test/ replies controller fixed

--- spec/controllers/replies_controller_spec.rb
+++ spec/controllers/replies_controller_spec.rb
@@ -7,12 +7,18 @@ RSpec.describe RepliesController, type: :controller do
     let(:question){create(:question, plain_user: user)}
     let(:reply){create(:reply, user: user, question: question)}
 
-    let(:request){post :create, params: {
-                    question_id: question,
-    reply: attributes_for(:reply, :plain_user_reply) }}
+    let(:request){
+      post :create, params: {
+        question_id: question,
+        reply: {
+          "email": user.email,
+          "content": "Hey-la-la-ley",
+          "user_type": "PlainUser"
+        }
+      }
+    }
 
     it 'is creates reply' do
-      expect(reply.user_email).to eq nil
       expect{ request }.to change(Reply, :count).by(1)
     end
 
